4b7c6d8ae28c2710dc46feb0171605577b1d763c
vishnudeviravi <ivishnudr@gmail.com>
2023-04-02 10:27:18 +0530

    adding changes

--- todoapp/todoapp/settings.py
+++ todoapp/todoapp/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crispy_forms',
-    'crispy_bootstrap5'
+    'crispy_bootstrap5',    
 ]
 
 MIDDLEWARE = [
